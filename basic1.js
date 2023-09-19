1d9213d2acbcecaceba27713447ee7ead349f652
Habiba Akter <mshabiba573@gmail.com>
2023-09-19 13:32:46 +0600

    all

---
+++ basic1.js
@@ -0,0 +1,94 @@
+// var num1 = 10;
+// var num2 = 20;
+// console.log(num1 + num2);
+
+var num1 = 10;
+var num2 = 20;
+var sum = num1 + num2;
+var subtraction
+// console.log('sum =', sum);
+// console.log(sum);
+let student1 = 'habiba'
+student1 = 'akter'
+const student2 = 'afif'
+
+student1 = 'habiba'
+// student2 = 'adib'
+
+// console.log(student2);
+let number1 = 20; 
+let number2 = 30;
+// let sum = number1 + number2;
+let sub = number2 - number1
+let multiplication = number1 * number2;
+let divition = number1 / number2;
+let remainer = number1 % number2;
+// console.log('sum =', sum); 
+// console.log('sub =', sub); 
+// console.log('multiplication =', multiplication); 
+// console.log('divition =', divition); 
+// console.log('remainer =', remainer); 
+
+let score_board = 200;
+score_board += 4;
+
+
+
+score_board =score_board + 5;
+score_board =score_board + 6;
+// console.log('score_board =', score_board);
+let bkashAccount = 2000;
+bkashAccount = bkashAccount + 1000;
+bkashAccount += 500;
+bkashAccount -= 500;
+bkashAccount = 500;
+
+// console.log('bkashAccount =', bkashAccount);
+
+let counter = 3;
+ counter++;
+ counter++;
+ counter--;
+//  console.log(counter);
+
+
+let firstName ='habiba'
+let lastName = 'akter'
+let fullName = firstName + " " + lastName;
+// console.log(fullName);
+let value1 = 10;
+let value2 = '30'
+let value2_convert_in_number = parseInt(value2)
+
+
+let totel = value1 + value2_convert_in_number;
+// console.log(totel);
+let  a = 0.1;
+let b = 0.2;
+let sum1 = a + b;
+// console.log(sum1);
+let convertfixed = sum1.toFixed(1)
+// console.log( "convert in one decimal",convertfixed);
+let convert_in_numberr = parseFloat(convertfixed)
+
+// let convert_in_tow_decimal_number =
+// console.log('convert in number',  convert_in_numberr);
+// console.log(value1);
+
+
+// if else comdition
+// let a1= 20;
+// let a2= 30;
+
+let password= 123456
+let user= "mshabiba"
+
+// login page
+
+if (123456 == password && "mshabiba@gmail.com" == user) {
+    console.log( 'showing you your profill');
+}
+
+else{
+    console.log('password wrong');
+}
